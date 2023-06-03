94c7094612da64fd2519d650fb12ffa01623e272
Ésley Nathan <31036923+Esleynathan@users.noreply.github.com>
2023-06-03 16:37:41 -0300

    Type_event | Cadastro - HTML, CSS, Views, Urls, Messages, Validações.

--- type_event/settings.py
+++ type_event/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,7 +46,7 @@ ROOT_URLCONF = 'type_event.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,9 +107,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static'),)
+STATIC_ROOT = os.path.join('static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Messages
+
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.WARNING: 'alert-warning',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info ',
+}
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.messages import constants
+
 
 def cadastro(request):
-    print('oi')
-    return HttpResponse('Estou no cadastro')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Usuário já existe.')            
+            return redirect(reverse('cadastro'))  
+
+        if not (senha == confirmar_senha):
+            messages.add_message(request, constants.ERROR, 'As senhas não coincidem.')
+            return redirect(reverse('cadastro'))
+        
+         
+        
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+        
+        messages.add_message(request, constants.SUCCESS, 'Usuário cadstrado com sucesso.')
+    return redirect(reverse('login'))
